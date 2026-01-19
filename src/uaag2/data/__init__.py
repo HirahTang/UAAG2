cd230c1b50ab295d828a950869654f2cea671220
Nikolaj Hey Hinnerskov <nhey@900901.xyz>
2026-01-19 14:41:12 +0100

    fix imports

---
+++ src/uaag2/data/__init__.py
@@ -0,0 +1,4 @@
+from uaag2.data.abstract_dataset import AbstractDataset
+from uaag2.data.uaag_dataset import UAAG2Dataset, UAAG2DataModule, Dataset_Info
+
+__all__ = ["AbstractDataset", "UAAG2Dataset", "UAAG2DataModule", "Dataset_Info"]
