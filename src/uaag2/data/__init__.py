58489c839eab36f73cde334c323e28b937dbe253
Nikolaj Hey Hinnerskov <nhey@900901.xyz>
2026-01-20 09:39:53 +0100

    purported import fix

--- src/uaag2/data/__init__.py
+++ src/uaag2/data/__init__.py
@@ -1,4 +1,3 @@
-from uaag2.data.abstract_dataset import AbstractDataset
 from uaag2.data.uaag_dataset import UAAG2Dataset, UAAG2DataModule, Dataset_Info
 
-__all__ = ["AbstractDataset", "UAAG2Dataset", "UAAG2DataModule", "Dataset_Info"]
+__all__ = ["UAAG2Dataset", "UAAG2DataModule", "Dataset_Info"]
