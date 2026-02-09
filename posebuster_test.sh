69d13dd7a7bdaf4f67c6c01d1e3ef65d10e332ee
hirahtang <tanghan1995@gmail.com>
2026-02-09 14:52:58 +0200

    PoseBuster test

--- posebuster_test.sh
+++ posebuster_test.sh
@@ -7,6 +7,7 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=20G
 #SBATCH --time=2-00:00:00
+#SBATCH --array=0-4
 #SBATCH -o logs/posebuster_%j.log
 #SBATCH -e logs/posebuster_%j.log
 
@@ -22,18 +23,33 @@ git checkout main
 # ============================================================================
 echo "→ Setting up environment..."
 
-WORK_DIR=/flash/project_465002574/UAAG2_main
-SAVE_DIR=/scratch/project_465002574/UNAAGI_archives/UAAG_model
 # Load modules for LUMI
 module load LUMI
 module load CrayEnv
 module load lumi-container-wrapper/0.4.2-cray-python-default
 export PATH="/flash/project_465002574/unaagi_env/bin:$PATH"
-cd ${SAVE_DIR}
-mkdir -p A0A247D711_LISMN_iter1_extracted && tar -xzf /scratch/project_465002574/UNAAGI_archives/UAAG_model/A0A247D711_LISMN_UAAG_model_1000_iter1_mol_files.tar.gz -C A0A247D711_LISMN_iter1_extracted
-cd ${WORK_DIR}
+
+mkdir -p /scratch/project_465002574/UAAG_logs
+
+MODEL="UAAG_model"
+NUM_SAMPLES=1000
+PROTEIN_ID="ARGR_ECOLI"
+# ============================================================================
+# CHECK SAMPLES DIRECTORY
+# ============================================================================
+BASE_PATH=/scratch/project_465002574/ProteinGymSampling
+RUN_DIR="run${MODEL}/${PROTEIN_ID}_${MODEL}_${NUM_SAMPLES}_iter${SLURM_ARRAY_TASK_ID}"
+SAMPLES_DIR="${BASE_PATH}/${RUN_DIR}"
+
+
+WORK_DIR=/flash/project_465002574/UAAG2_main
+
+SAVE_DIR=/scratch/project_465002574/UNAAGI_archives/UAAG_model
+OUTPUT_CSV="${SAMPLES_DIR}/PoseBusterResults"
+TEMP_DIR="/flash/project_465002574/temp_sdf_posebuster_${SLURM_ARRAY_JOB_ID}_iter${SLURM_ARRAY_TASK_ID}"
+
 
 python scripts/evaluate_mol_samples.py \
-    --input-dir /scratch/project_465002574/UNAAGI_archives/UAAG_model/A0A247D711_LISMN_iter1_extracted \
-    --output /scratch/project_465002574/UNAAGI_archives/UAAG_model/A0A247D711_LISMN_iter1_extracted/PoseBusterResults \
-    --temp-dir /flash/project_465002574/temp_sdf_iter1
+    --input-dir "${SAMPLES_DIR}" \
+    --output "${OUTPUT_CSV}" \
+    --temp-dir "${TEMP_DIR}"
