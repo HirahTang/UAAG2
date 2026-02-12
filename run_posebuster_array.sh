2492b8c1a29e363f76509874a68907655e572aec
hirahtang <tanghan1995@gmail.com>
2026-02-12 17:25:17 +0200

    ILE training

--- run_posebuster_array.sh
+++ run_posebuster_array.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #SBATCH --job-name=UAAG_posebuster
 #SBATCH --account=project_465002574
-#SBATCH --partition=standard-g
+#SBATCH --partition=small-g
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=20G
 #SBATCH --time=2-00:00:00
-#SBATCH --array=0-1
+#SBATCH --array=4
 #SBATCH -o /scratch/project_465002574/UAAG_logs/posebuster_%A_%a.log
 #SBATCH -e /scratch/project_465002574/UAAG_logs/posebuster_%A_%a.log
 
